cf1f1e4228f0b00ba24c936c0bfede52b7a8855d
lucasjsanti <lucas_swatoficial@hotmail.com>
2022-12-07 18:31:08 -0300

    primeiro commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function logar() {
+
+    var usuario = document.getElementById ('user').value;
+    var senha = document.getElementById ('password').value;
+    var listaUsuario= []
+
+
+}
+
+
+
+
+
+
